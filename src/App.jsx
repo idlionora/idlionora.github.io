1379c27f486ec2ea024cc3b822dc63d96215c510
Nurul Azizah <idlionora@gmail.com>
2023-06-02 23:10:04 +0700

    feat(Home): add profile pictures and decorative svg component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from './components/Navbar';
+import ProfileTitle from './assets/ProfileTitle';
 import './App.css'
+
 
 function App() {
   return (
 		<>
 			<Navbar />
-			<main>
-				<section id="about" className='w-full h-96 bg-pink-300'></section>
+			<main className='pt-14'>
+				<section id="about" className='w-full h-96 bg-pink-300 text-blue-400'>
+					<ProfileTitle width="100%"/>
+				</section>
 				<section id="skills" className='w-full h-96 bg-blue-200'></section>
 				<section id="projects" className='w-full h-96 bg-yellow-100'></section>
 				<section id="contact-me" className='w-full h-96 bg-theme-purple opacity-30'></section>
